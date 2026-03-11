d02e896d55130f0989d236f323e8ed812cdb58f9
Денис Ткачёв <denis.tkachev.07@gmail.com>
2026-03-11 19:20:59 +0700

    изменено:      README.md 	новый файл:    stroka 	изменено:      stroka.cpp

--- stroka.cpp
+++ stroka.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
+#include <clocale>
+using namespace std;
 
 int main(){
-    std::cout << "fhksdhfksdf";
+    setlocale(LC_ALL, "ru_RU.UTF-8"); // чтоб видел русские буквы
+
+    wstring vowels = L"АаОоУуЫыЭэИиЯяЁёЕеЮюAaOoYyUuEeIi"; 
+    cout << "Наберите строку на русском или английском языке: "<< endl;
+    wstring inStr; // строка ввода
+    getline(wcin, inStr);
+    if (inStr.size() != 0){ // проверка на пустую строку
+
+        int num = 0; // количество гласных
+                 
+        for (size_t i = 0; i < inStr.size(); i++){
+            for (size_t  y = 0; y  < vowels.size(); y++){
+                if (inStr[i] == vowels[y]){ // проверка каждого символа на наличие его в vowels
+                    num++;
+                    break; // остановка цикла если буква уже найдена
+                }
+            }
+        }   
+    cout << "Всего гласных букв строке: " << num << endl;
+    }
+    else {
+        cout << "Строка пуста, гласных нет.";
+    }
     return 0;
 }
