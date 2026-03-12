0e00b356f36ea320e73b884d99f9258fb4e453dd
Денис Ткачёв <denis.tkachev.07@gmail.com>
2026-03-12 18:13:41 +0700

    Изменеие кода, добавление работы через функцию

--- stroka.cpp
+++ stroka.cpp
@@ -3,13 +3,7 @@
 #include <clocale>
 using namespace std;
 
-int main(){
-    setlocale(LC_ALL, "ru_RU.UTF-8"); // чтоб видел русские буквы
-
-    wstring vowels = L"АаОоУуЫыЭэИиЯяЁёЕеЮюAaOoYyUuEeIi"; 
-    cout << "Наберите строку на русском или английском языке: "<< endl;
-    wstring inStr; // строка ввода
-    getline(wcin, inStr);
+int funcNumVowels(const wstring& inStr, const wstring& vowels){
     if (inStr.size() != 0){ // проверка на пустую строку
 
         int num = 0; // количество гласных
@@ -22,10 +16,28 @@ int main(){
                 }
             }
         }   
+    return num;
+    }
+    return 0;
+}
+
+
+int main(){
+    setlocale(LC_ALL, "ru_RU.UTF-8"); // чтоб видел русские буквы
+
+    wstring vowels = L"АаОоУуЫыЭэИиЯяЁёЕеЮюAaOoYyUuEeIi"; 
+    cout << "Наберите строку на русском или английском языке: "<< endl;
+
+    wstring inStr; // строка ввода
+    getline(wcin, inStr);
+
+    if (inStr.size() != 0){    
+    int num = funcNumVowels(inStr, vowels);
     cout << "Всего гласных букв строке: " << num << endl;
     }
     else {
         cout << "Строка пуста, гласных нет.";
     }
+
     return 0;
 }
